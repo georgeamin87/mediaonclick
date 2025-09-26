1746b0595ac3be8ba19a64117883d98a9f4f9d76
Zozelking <dr.george.ramzy@gmail.com>
2025-09-26 18:28:09 +0400

    team hana

--- src/app/ui/Slider/TeamSlider.jsx
+++ src/app/ui/Slider/TeamSlider.jsx
@@ -27,7 +27,7 @@ const teamData = [
     },
   },
   {
-    memberImage: '/images/team/hanna.jpg',
+    memberImage: '/images/team/Hanna.jpg',
     memberName: 'Hanna Mae Dichoso',
     memberDesignation: 'Executive Assistant',
     memberSocial: {
