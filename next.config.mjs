5c6c1ae50e0e1f57fa79b6f002b43e20431a39dc
George Amin <dr.george.amin87@gmail.com>
2026-08-12 13:51:18 +0400

    Fix of sitemap redirection

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,65 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.mediaonclick.me',
+          },
+        ],
+        destination: 'https://mediaonclick.me/:path*',
+        permanent: true,
+      },
+      {
+        source: '/about/media-partners',
+        destination: '/our-clients',
+        permanent: true,
+      },
+      {
+        source: '/about/news',
+        destination: '/blog',
+        permanent: true,
+      },
+      {
+        source: '/about/clientele',
+        destination: '/our-clients',
+        permanent: true,
+      },
+      {
+        source: '/what-we-do/case-studies',
+        destination: '/portfolio',
+        permanent: true,
+      },
+      {
+        source: '/what-we-do/case-studies/cfi',
+        destination: '/portfolio',
+        permanent: true,
+      },
+      {
+        source: '/home',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/services',
+        destination: '/service',
+        permanent: true,
+      },
+      {
+        source: '/work-with-us/sign-up-influencer',
+        destination: '/service/influencers',
+        permanent: true,
+      },
+      {
+        source: '/work-with-us',
+        destination: '/contact',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
